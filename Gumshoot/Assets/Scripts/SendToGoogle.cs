ce2dc82d94f83e5b5ce1859f3b91ced3993d40b0
delluluwxy <xiaoyingwang7@outlook.com>
2024-10-22 03:20:50 -0700

    add second metrics

--- Gumshoot/Assets/Scripts/SendToGoogle.cs
+++ Gumshoot/Assets/Scripts/SendToGoogle.cs
@@ -2,13 +2,16 @@ using System;
 using System.Collections;
 using UnityEngine;
 using UnityEngine.Networking;
+using static UnityEditor.FilePathAttribute;
 
 public class SendToGoogle : MonoBehaviour
 {
     [SerializeField] private string URL;
     private long _sessionID;
     private string _deathsPerCheckpoint;
-    private Vector3 _deathLocation;
+    private string _deathLocationsLevel0;
+    private string _deathLocationsLevel1;
+    private string _deathLocationsLevelMain;
     private string _checkpointOrder;
     private int _timesUseEnemyAbility;
 
@@ -26,22 +29,24 @@ public class SendToGoogle : MonoBehaviour
     public void Send()
     {
         _deathsPerCheckpoint = string.Join(", ", LevelManager.deathPerCheckpoint);
-
-        _deathLocation = new Vector3(0,0,0);
+        _deathLocationsLevel0 = string.Join(", ", LevelManager.deathLocationListLevel0.ConvertAll(v => $"{v.x}, {v.y}, {v.z}"));
+        _deathLocationsLevel1 = string.Join(", ", LevelManager.deathLocationListLevel1.ConvertAll(v => $"{v.x}, {v.y}, {v.z}"));
+        _deathLocationsLevelMain = string.Join(", ", LevelManager.deathLocationListLevelMain.ConvertAll(v => $"{v.x}, {v.y}, {v.z}"));
         _checkpointOrder = "01234";
         _timesUseEnemyAbility = 2;
 
-        StartCoroutine(Post(_sessionID.ToString(), _deathsPerCheckpoint, _deathLocation.ToString(), _checkpointOrder, _timesUseEnemyAbility.ToString()));
+        StartCoroutine(Post(_sessionID.ToString(), _deathsPerCheckpoint, _deathLocationsLevel0, _deathLocationsLevel1, _deathLocationsLevelMain, _checkpointOrder, _timesUseEnemyAbility.ToString()));
     }
 
 
-    private IEnumerator Post(string sessionID, string _deathsPerCheckpoint, string _deathLocation, string _checkpointOrder, string _timesUseEnemyAbility)
+    private IEnumerator Post(string sessionID, string _deathsPerCheckpoint, string _deathLocationsLevel0, string _deathLocationsLevel1, string _deathLocationsLevelMain, string _checkpointOrder, string _timesUseEnemyAbility)
     {
+        string _allDeathLocations = $"{_deathLocationsLevel0} | {_deathLocationsLevel1} | {_deathLocationsLevelMain}";
         // Create the form and enter responses
         WWWForm form = new WWWForm();
         form.AddField("entry.835694260", sessionID);
         form.AddField("entry.356227542", _deathsPerCheckpoint);
-        form.AddField("entry.1741040447", _deathLocation);
+        form.AddField("entry.1741040447", _allDeathLocations);
         form.AddField("entry.1026778936", _checkpointOrder);
         form.AddField("entry.2067514501", _timesUseEnemyAbility);
 
