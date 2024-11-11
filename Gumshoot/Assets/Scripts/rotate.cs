c7cda747247cbad526bb3d16085e32afb58fc228
Xiang Huang <xhuang41@usc.edu>
2024-11-10 19:17:49 -0800

    update-change the rotate scripts back and add rotate icons

--- Gumshoot/Assets/Scripts/rotate.cs
+++ Gumshoot/Assets/Scripts/rotate.cs
@@ -6,43 +6,7 @@ public class Rotate : MonoBehaviour
 {
     public float rotationSpeed = 50.0f;
     private bool isSpinning = false;
-    private GameObject centerAxis;
-
-    void Start()
-    {
-        CreateCenterAxis();
-    }
-
-    private void CreateCenterAxis()
-    {
-        centerAxis = new GameObject("CenterAxis");
-        centerAxis.transform.parent = transform;
-        centerAxis.transform.localPosition = Vector3.zero;
-
-        SpriteRenderer sr = centerAxis.AddComponent<SpriteRenderer>();
-        sr.sprite = CreateBlackSquareSprite();
-        sr.sortingOrder = 1;
-
-        centerAxis.transform.localScale = new Vector3(1f, 1f, 1f); 
-    }
-
-    private Sprite CreateBlackSquareSprite()
-    {
-        Texture2D texture = new Texture2D(64, 64, TextureFormat.ARGB32, false);
-
-        for (int x = 0; x < texture.width; x++)
-        {
-            for (int y = 0; y < texture.height; y++)
-            {
-                texture.SetPixel(x, y, Color.black);
-            }
-        }
-        texture.Apply();
-
-        Rect rect = new Rect(0, 0, texture.width, texture.height);
-        return Sprite.Create(texture, rect, new Vector2(0.5f, 0.5f));
-    }
-
+    
     public void StartFan()
     {
         isSpinning = true;
