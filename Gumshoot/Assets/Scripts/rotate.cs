72e14f309c3f46a111190ac96c934c5cb2c940de
YtG12 <151299641+YtG12@users.noreply.github.com>
2024-11-10 19:52:09 -0800

    Updated icons for all rotating platforms

--- Gumshoot/Assets/Scripts/rotate.cs
+++ Gumshoot/Assets/Scripts/rotate.cs
@@ -1,12 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Rotate : MonoBehaviour
 {
     public float rotationSpeed = 50.0f;
     private bool isSpinning = false;
-    
+
     public void StartFan()
     {
         isSpinning = true;
@@ -22,11 +23,16 @@ public class Rotate : MonoBehaviour
         if (isSpinning)
         {
             transform.Rotate(Vector3.forward, rotationSpeed * Time.deltaTime);
+
+            Transform canvasTransform = transform.Find("Canvas");
+            if (canvasTransform != null)
+            {
+                Image imageComponent = canvasTransform.GetComponentInChildren<Image>();
+                if (imageComponent != null)
+                {
+                    imageComponent.color = Color.green;
+                }
+            }
         }
     }
 }
-
-
-
-
-
