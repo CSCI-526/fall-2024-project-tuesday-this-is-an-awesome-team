7bc2f4fea672c1a8c95a832c93ff9626ecae9c59
YtG12 <151299641+YtG12@users.noreply.github.com>
2024-11-22 16:39:08 -0800

    Fixed HelpContentManager initialization

--- Gumshoot/Assets/Scripts/HelpContentManager.cs
+++ Gumshoot/Assets/Scripts/HelpContentManager.cs
@@ -7,122 +7,127 @@ public class HelpContentManager : MonoBehaviour
 
     private void Awake()
     {
-        Instance = this; 
+        Instance = this;
+        InitializeLevelHelpContent();
     }
 
     [System.Serializable]
     public class HelpRule
     {
-        public Sprite icon;  
-        public string text;  
+        public Sprite icon;
+        public string text;
     }
 
     [System.Serializable]
     public class LevelHelpData
     {
-        public List<HelpRule> rules = new List<HelpRule>(); 
+        public List<HelpRule> rules = new List<HelpRule>();
     }
 
-    public Dictionary<string, LevelHelpData> levelHelpContent = new Dictionary<string, LevelHelpData>()
-    {
-        {
-            "Level 1",
-            new LevelHelpData
-            {
-                rules = new List<HelpRule>
-                {
-                    new HelpRule { icon = Instance.mouseLeftIcon, text = "stick" }
-                }
-            }
-        },
-        {
-            "Level 2",
-            new LevelHelpData
-            {
-                rules = new List<HelpRule>
-                {
-                    new HelpRule { icon = Instance.mouseLeftIcon, text = "stick/grab" },
-                    new HelpRule { icon = Instance.mouseRightIcon, text = "throw" },
-                    new HelpRule { icon = Instance.keyboardEIcon, text = "fall" }
-                }
-            }
-        },
-        {
-            "Level 3",
-            new LevelHelpData
-            {
-                rules = new List<HelpRule>
-                {
-                    new HelpRule { icon = Instance.mouseLeftIcon, text = "stick/grab" },
-                    new HelpRule { icon = Instance.mouseRightIcon, text = "throw" },
-                    new HelpRule { icon = Instance.keyboardEIcon, text = "fall" },
-                    new HelpRule { icon = Instance.keyboardMIcon, text = "switch map view" },
-                    new HelpRule { icon = Instance.keyboardArrowKeysIcon, text = "flying enemy-move" }
-                }
-            }
-        },
-        {
-            "Level 4",
-            new LevelHelpData
-            {
-                rules = new List<HelpRule>
-                {
-                    new HelpRule { icon = Instance.mouseLeftIcon, text = "stick/grab" },
-                    new HelpRule { icon = Instance.mouseRightIcon, text = "throw" },
-                    new HelpRule { icon = Instance.keyboardEIcon, text = "fall" },
-                    new HelpRule { icon = Instance.keyboardMIcon, text = "switch map view" },
-                    new HelpRule { icon = Instance.keyboardSpaceIcon, text = "shooting enemy-shoot" }
-                }
-            }
-        },
-        {
-            "Level 5",
-            new LevelHelpData
-            {
-                rules = new List<HelpRule>
-                {
-                    new HelpRule { icon = Instance.mouseLeftIcon, text = "stick/grab" },
-                    new HelpRule { icon = Instance.mouseRightIcon, text = "throw" },
-                    new HelpRule { icon = Instance.keyboardMIcon, text = "switch map view" },
-                    new HelpRule { icon = Instance.checkPointIcon, text = "Save level progress" },
-                    new HelpRule { icon = Instance.keyboardEIcon, text = "fall/jumping enemy-jump" }
-                }
-            }
-        },
-        {
-            "Level 6",
-            new LevelHelpData
-            {
-                rules = new List<HelpRule>
-                {
-                    new HelpRule { icon = Instance.mouseLeftIcon, text = "stick/grab" },
-                    new HelpRule { icon = Instance.mouseRightIcon, text = "throw" },
-                    new HelpRule { icon = Instance.keyboardMIcon, text = "switch map view" },
-                    new HelpRule { icon = Instance.checkPointIcon, text = "Save level progress" },
-                    new HelpRule { icon = Instance.keyboardEIcon, text = "fall/jumping enemy-jump" },
-                    new HelpRule { icon = Instance.keyboardArrowKeysIcon, text = "flying enemy-move" },
-                    new HelpRule { icon = Instance.keyboardSpaceIcon, text = "shooting enemy-shoot" },
-                }
-            }
-        },
-        {
-            "Level 7",
-            new LevelHelpData
-            {
-                rules = new List<HelpRule>
-                {
-                    new HelpRule { icon = Instance.mouseLeftIcon, text = "stick/grab" },
-                    new HelpRule { icon = Instance.mouseRightIcon, text = "throw" },
-                    new HelpRule { icon = Instance.keyboardMIcon, text = "switch map view" },
-                    new HelpRule { icon = Instance.checkPointIcon, text = "Save level progress" },
-                    new HelpRule { icon = Instance.keyboardEIcon, text = "fall/jumping enemy-jump" },
-                    new HelpRule { icon = Instance.keyboardArrowKeysIcon, text = "flying enemy-move" },
-                    new HelpRule { icon = Instance.keyboardSpaceIcon, text = "shooting enemy-shoot" },
-                }
-            }
-        }
-    };
+    public Dictionary<string, LevelHelpData> levelHelpContent;
 
+    private void InitializeLevelHelpContent()
+    {
+        levelHelpContent = new Dictionary<string, LevelHelpData>()
+        {
+            {
+                "Level 1",
+                new LevelHelpData
+                {
+                    rules = new List<HelpRule>
+                    {
+                        new HelpRule { icon = Instance.mouseLeftIcon, text = "stick" }
+                    }
+                }
+            },
+            {
+                "Level 2",
+                new LevelHelpData
+                {
+                    rules = new List<HelpRule>
+                    {
+                        new HelpRule { icon = Instance.mouseLeftIcon, text = "stick/grab" },
+                        new HelpRule { icon = Instance.mouseRightIcon, text = "throw" },
+                        new HelpRule { icon = Instance.keyboardEIcon, text = "fall" }
+                    }
+                }
+            },
+            {
+                "Level 3",
+                new LevelHelpData
+                {
+                    rules = new List<HelpRule>
+                    {
+                        new HelpRule { icon = Instance.mouseLeftIcon, text = "stick/grab" },
+                        new HelpRule { icon = Instance.mouseRightIcon, text = "throw" },
+                        new HelpRule { icon = Instance.keyboardEIcon, text = "fall" },
+                        new HelpRule { icon = Instance.keyboardMIcon, text = "switch map view" },
+                        new HelpRule { icon = Instance.keyboardArrowKeysIcon, text = "flying enemy-move" }
+                    }
+                }
+            },
+            {
+                "Level 4",
+                new LevelHelpData
+                {
+                    rules = new List<HelpRule>
+                    {
+                        new HelpRule { icon = Instance.mouseLeftIcon, text = "stick/grab" },
+                        new HelpRule { icon = Instance.mouseRightIcon, text = "throw" },
+                        new HelpRule { icon = Instance.keyboardEIcon, text = "fall" },
+                        new HelpRule { icon = Instance.keyboardMIcon, text = "switch map view" },
+                        new HelpRule { icon = Instance.keyboardSpaceIcon, text = "shooting enemy-shoot" }
+                    }
+                }
+            },
+            {
+                "Level 5",
+                new LevelHelpData
+                {
+                    rules = new List<HelpRule>
+                    {
+                        new HelpRule { icon = Instance.mouseLeftIcon, text = "stick/grab" },
+                        new HelpRule { icon = Instance.mouseRightIcon, text = "throw" },
+                        new HelpRule { icon = Instance.keyboardMIcon, text = "switch map view" },
+                        new HelpRule { icon = Instance.checkPointIcon, text = "Save level progress" },
+                        new HelpRule { icon = Instance.keyboardEIcon, text = "fall/jumping enemy-jump" }
+                    }
+                }
+            },
+            {
+                "Level 6",
+                new LevelHelpData
+                {
+                    rules = new List<HelpRule>
+                    {
+                        new HelpRule { icon = Instance.mouseLeftIcon, text = "stick/grab" },
+                        new HelpRule { icon = Instance.mouseRightIcon, text = "throw" },
+                        new HelpRule { icon = Instance.keyboardMIcon, text = "switch map view" },
+                        new HelpRule { icon = Instance.checkPointIcon, text = "Save level progress" },
+                        new HelpRule { icon = Instance.keyboardEIcon, text = "fall/jumping enemy-jump" },
+                        new HelpRule { icon = Instance.keyboardArrowKeysIcon, text = "flying enemy-move" },
+                        new HelpRule { icon = Instance.keyboardSpaceIcon, text = "shooting enemy-shoot" },
+                    }
+                }
+            },
+            {
+                "Level 7",
+                new LevelHelpData
+                {
+                    rules = new List<HelpRule>
+                    {
+                        new HelpRule { icon = Instance.mouseLeftIcon, text = "stick/grab" },
+                        new HelpRule { icon = Instance.mouseRightIcon, text = "throw" },
+                        new HelpRule { icon = Instance.keyboardMIcon, text = "switch map view" },
+                        new HelpRule { icon = Instance.checkPointIcon, text = "Save level progress" },
+                        new HelpRule { icon = Instance.keyboardEIcon, text = "fall/jumping enemy-jump" },
+                        new HelpRule { icon = Instance.keyboardArrowKeysIcon, text = "flying enemy-move" },
+                        new HelpRule { icon = Instance.keyboardSpaceIcon, text = "shooting enemy-shoot" },
+                    }
+                }
+            }
+        };
+    }
     public Sprite mouseLeftIcon;
     public Sprite mouseRightIcon;
     public Sprite keyboardEIcon;
